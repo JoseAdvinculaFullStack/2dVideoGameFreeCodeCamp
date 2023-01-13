da45b7c1353bb643497419acfac6e166024cf842
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-01-13 16:27:55 -0500

    updateWindAndLose

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ window.addEventListener('load',function(){
     class UI{
         constructor(game){
             this.game=game;
-            this.fontSize=25;
+            this.fontSize= 25 ;
             this.fontFamily='Helvetica';
             this.color='white';
         }
@@ -138,16 +138,35 @@ window.addEventListener('load',function(){
             context.shadowOffsetX=2;
             context.shadowOffsetY=2;
             context.shadowColor='black';
-            context.font=this.fontSize+'px'+this.fontFamily;
+            context.font= this.fontSize + 'px ' + this.fontFamily;
 
             //score
-            context.fillText('Score:'+this.game.score,20,40);
+            context.fillText('Score: '+this.game.score,20,40);
             //amno
     
             for(let i=0;i<this.game.amno;i++){
                 context.fillRect(20+5*i,50,3,20);
             }
 
+            //game over messages
+            if(this.game.gameOver){
+                context.textAlign='center';
+                let message1;
+                let message2;
+                if(this.game.score>this.game.winningScore){
+                    message1='You Win!';
+                    message2='Well done!';
+                }else{
+                    message1='You Lose';
+                    message2='Try again next time';
+                }
+                context.font='50px '+this.fontFamily;
+                context.fillText(message1,this.game.width*0.5,this.game.height*0.5-40);
+                context.font='25px'+this.fontFamily;
+                context.fillText(message2,this.game.width*0.5,this.game.height*0.5+40);
+                
+            }
+            context.restore()
         }
     }
     class Game {
