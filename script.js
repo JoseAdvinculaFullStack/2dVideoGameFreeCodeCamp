d7a319d4bcc076367c168552fc56779bc39c0a15
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-01-18 17:31:19 -0500

    updateCollectiongPowerUps

--- script.js
+++ script.js
@@ -65,8 +65,11 @@ window.addEventListener('load',function(){
             this.maxSpeed=3;
             this.proyectiles=[];
             this.image=document.getElementById('player');
+            this.powerUp=false;
+            this.powerUpTimer=0;
+            this.powerUpLimit=10000;
         }
-        update(){
+        update(deltaTime){
             if(this.game.keys.includes('ArrowUp')) this.speedY=-this.maxSpeed;
             else if(this.game.keys.includes('ArrowDown')) this.speedY=this.maxSpeed;
             else this.speedY=0;
@@ -82,21 +85,46 @@ window.addEventListener('load',function(){
             }else{
                 this.frameX=0;
             }
+            // power up
+            if(this.powerUp){
+                if(this.powerUpTimer>this.powerUpLimit){
+                    this.powerUpTimer=0;
+                    this.powerUp=false;
+                    this.frameY=0;
+                }else{
+                    this.powerUpTimer+=deltaTime;
+                    this.frameY=1;
+                    this.game.amno += 0.1;
+                }
+            }
         }
         draw(context){
-           
-            if(this.game.debug)context.strokeRect(this.x,this.y,this.width,this.height);
-            context.drawImage(this.image,this.frameX*this.width,this.frameY*this.height,this.width,this.height,this.x,this.y,this.width,this.height);
             this.proyectiles.forEach(proyectiles=>{
                 proyectiles.draw(context);
             });
+            if(this.game.debug)context.strokeRect(this.x,this.y,this.width,this.height);
+            context.drawImage(this.image,this.frameX*this.width,this.frameY*this.height,this.width,this.height,this.x,this.y,this.width,this.height);
+          
         }
         shootTop(){
             if(this.game.amno>0){
                 this.proyectiles.push(new Proyectile(this.game,this.x +80 ,this.y +30)); 
                 this.game.amno--;
             }
+            if(this.powerUp) this.shootBottom();
            
+        }
+
+        shootBottom(){
+            if(this.game.amno>0){
+                this.proyectiles.push(new Proyectile(this.game,this.x+80,this.y+175));
+            }
+        }
+
+        enterPowerUp(){
+            this.powerUpTimer=0;
+            this.powerUp=true;
+            this.game.amno=this.game.maxAmno;
         }
     }
     class Enemy{
@@ -234,11 +262,6 @@ window.addEventListener('load',function(){
 
             //score
             context.fillText('Score: '+this.game.score,20,40);
-            //amno
-    
-            for(let i=0;i<this.game.amno;i++){
-                context.fillRect(20+5*i,50,3,20);
-            }
             //timer
             const formattedTime=(this.game.gameTime*0.001).toFixed(1);
             //toFixed es para generar la cantidad de decimales que queremos mostrar
@@ -262,6 +285,13 @@ window.addEventListener('load',function(){
                 context.fillText(message2,this.game.width*0.5,this.game.height*0.5+40);
                 
             }
+              //amno
+              if(this.game.player.powerUp) context.fillStyle='#ffffbd';
+
+              for(let i=0;i<this.game.amno;i++){
+                  context.fillRect(20+5*i,50,3,20);
+              }
+              //
             context.restore()
         }
     }
@@ -295,7 +325,7 @@ window.addEventListener('load',function(){
             if(this.gameTime>this.timeLimit) this.gameOver=true;
             this.background.update();
             this.background.Layer4.update();
-            this.player.update();
+            this.player.update(deltaTime);
             if(this.amnoTimer>this.amnoInterval){
                 if(this.amno<this.maxAmno) this.amno++;
                 this.amnoTimer=0;
@@ -306,6 +336,8 @@ window.addEventListener('load',function(){
                     enemy.update();
                     if(this.checkCollision(this.player,enemy)){
                         enemy.markedForDeletion=true;
+                        if(enemy.type=='lucky') {this.player.enterPowerUp()}
+                        else{ this.score--;}
                     }
                     this.player.proyectiles.forEach(projectile =>{
                         if(this.checkCollision(projectile,enemy)){
