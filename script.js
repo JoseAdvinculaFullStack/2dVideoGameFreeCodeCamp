31b7f319514dad7788f33993780c0f33f643619d
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-01-21 07:42:41 -0500

    updateTweaskAndFixes

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ window.addEventListener('load',function(){
             this.height=169;
             this.lives=2;
             this.score=this.lives;
-            this.y=Math.random()*(this.game.height*0.9-this.height);
+            this.y=Math.random()*(this.game.height*0.95-this.height);
             this.image=document.getElementById("angler1");
             this.frameY=Math.floor(Math.random()*3);
 
@@ -230,7 +230,7 @@ window.addEventListener('load',function(){
             this.height=165;
             this.lives=3;
             this.score=this.lives;
-            this.y=Math.random()*(this.game.height*0.9-this.height);
+            this.y=Math.random()*(this.game.height*0.95-this.height);
             this.image=document.getElementById("angler2");
             this.frameY=Math.floor(Math.random()*2);
 
@@ -244,10 +244,26 @@ window.addEventListener('load',function(){
             this.height=95;
             this.lives=3;
             this.score=15;
-            this.y=Math.random()*(this.game.height*0.9-this.height);
+            this.y=Math.random()*(this.game.height*0.95-this.height);
             this.image=document.getElementById("lucky");
             this.frameY=Math.floor(Math.random()*2);
             this.type='lucky';
+
+        }
+    }
+
+    class HiveWhale extends Enemy{
+        constructor(game){
+            super(game);
+            this.width=400;
+            this.height=227;
+            this.lives=15;
+            this.score=this.lives;
+            this.y=Math.random()*(this.game.height*0.95-this.height);
+            this.image=document.getElementById("hivewhale");
+            this.frameY=0;
+            this.type='hive';
+            this.speedX=Math.random()*-1.2-0.2;
 
         }
     }
@@ -439,6 +455,7 @@ window.addEventListener('load',function(){
         addEnemy(){
             const randomize=Math.random();
             if(randomize<0.3) this.enemies.push(new Angler1(this)); else if(randomize<0.6) this.enemies.push(new Angler2(this));
+            else if(randomize<0.8) this.enemies.push(new HiveWhale(this));
             else this.enemies.push(new Lucky(this));      
         }
         checkCollision(rect1,rect2){
