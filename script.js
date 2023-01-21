07387af65a412357d7706cfa5d9277ab2934ad85
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-01-21 07:00:11 -0500

    updateParticleRotation

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ window.addEventListener('load',function(){
         update(){
             this.angle+=this.va;
             this.speedY+=this.gravity;
-            this.x-=this.speedX;
+            this.x-=this.speedX - this.game.speed;
             this.y+=this.speedY;
             if(this.y>this.game.height+this.size || this.x< 0-this.size ) this.markedForDeletion=true;
             if(this.y>this.game.height-this.bottomBounceBoundary && this.bounced<2){
@@ -85,7 +85,12 @@ window.addEventListener('load',function(){
         }
         draw(context)
         {
-            context.drawImage(this.image,this.frameX*this.spriteSize,this.frameY*this.spriteSize,this.spriteSize,this.spriteSize,this.x,this.y,this.size,this.size)
+
+            context.save();
+            context.translate(this.x,this.y);
+            context.rotate(this.angle);
+            context.drawImage(this.image,this.frameX*this.spriteSize,this.frameY*this.spriteSize,this.spriteSize,this.spriteSize,0,0,this.size,this.size)
+            context.restore();
         }
 
 
